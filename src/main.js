5e0f43b83b108912b5b96fdc4d1bdc381d56e9f7
consatanos <constantoskin@gmail.com>
2020-03-31 17:07:14 +0300

    add create trip-info template function

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 'use strict';
 
+const createTripInfoTemplate = () => {
+  return (
+    `<section class="trip-main__trip-info  trip-info">
+      <div class="trip-info__main">
+        <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+        <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+      </div>
+    </section>`
+  );
+};
+
 const createSiteMenuTemplate = () => {
   return (
     `<nav class="trip-controls__trip-tabs  trip-tabs">
