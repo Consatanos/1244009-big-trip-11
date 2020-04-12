5fe0c4f3d6892732da0c3da2273f06f409dbca3c
consatanos <constantoskin@gmail.com>
2020-04-12 19:45:22 +0300

    add filter mock

---
+++ src/mock/filters.js
@@ -0,0 +1,18 @@
+/**
+ * Array filter names
+ */
+const filterNames = [
+  `everything`,
+  `future`,
+  `past`,
+];
+
+/**
+ * Generate filters array
+ * @return {array} array of filters
+ */
+const generateFilters = () => filterNames;
+
+export {
+  generateFilters,
+};
