7bda08919972dc88267232e9302ce0be6bbdb3ce
consatanos <constantoskin@gmail.com>
2020-04-12 19:45:58 +0300

    add const

---
+++ src/const.js
@@ -0,0 +1,55 @@
+/**
+ * Array route values
+ */
+const ROUTE_ITEMS = [
+  `Taxi`,
+  `Bus`,
+  `Train`,
+  `Ship`,
+  `Transport`,
+  `Drive`,
+  `Flight`,
+  `Check-in`,
+  `Sightseeng`,
+  `Restaurant`,
+];
+
+/**
+ * Array route action values
+ */
+const ROUTE_ACTION = [
+  `to`,
+  `in`,
+];
+
+/**
+ * Array currency values
+ */
+const CURRENCY = [
+  `&euro;`,
+];
+
+/**
+ * Array names 0f month
+ */
+const NAMES_OF_MONTH = [
+  `Jan`,
+  `Feb`,
+  `Mar`,
+  `Apr`,
+  `May`,
+  `Jun`,
+  `Jul`,
+  `Aug`,
+  `Sept`,
+  `Oct`,
+  `Nov`,
+  `Dec`,
+];
+
+export {
+  ROUTE_ITEMS,
+  ROUTE_ACTION,
+  CURRENCY,
+  NAMES_OF_MONTH,
+};
