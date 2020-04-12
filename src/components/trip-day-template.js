ff6b7bad73cae4e53f1078b13e8b36c09481d75e
consatanos <constantoskin@gmail.com>
2020-04-12 19:50:26 +0300

    update trip-day-template for mock

--- src/components/trip-day-template.js
+++ src/components/trip-day-template.js
@@ -1,20 +1,34 @@
+import {
+  NAMES_OF_MONTH,
+} from '../const';
+import {
+  routeTemplate,
+} from './route-template';
+
 /**
  * Create template trip-day
+ * @param {string} date
+ * @param {array} routes
  * @return {string} html template trip-day
  */
-const tripDayTemplate = () => {
+const tripDayTemplate = (date, routes) => {
+  const formalDate = new Date(date);
+
   return (
     `<li class="trip-days__item  day">
       <div class="day__info">
-        <span class="day__counter">1</span>
-        <time class="day__date" datetime="2019-03-18">MAR 18</time>
+        <span class="day__counter">${formalDate.getDate()}</span>
+        <time class="day__date" datetime="${date}">${NAMES_OF_MONTH[formalDate.getMonth()]} ${formalDate.getFullYear().toString().substr(-2)}</time>
       </div>
       <ul class="trip-events__list">
+
+        ${routes.map((route) => routeTemplate(route))}
+
       </ul>
     </li>`
   );
 };
 
 export {
-  tripDayTemplate
+  tripDayTemplate,
 };
